fd98177a38108ff2fb437fc937c2ad394ab6e237
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-02-17 20:57:31 -0500

    Added icons -Sud

--- unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
+++ unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
@@ -5,30 +5,33 @@ import email_icon from '../Assets/email.png'
 import lock_icon from '../Assets/lock.png'
 import person_icon from '../Assets/person.png'
 const LoginSignup = () => {
-    return(
+    return (
         <div className='container'>
             <div className="header">
                 <div className="text">Sign Up</div>
                 <div className="underline"></div>
             </div>
-            <div className="input"></div>
-            <div className="input">
-                <img src={person_icon} alt=""/>
-                <input type="text"/>
+            <div className="inputs">
+                <div className="input">
+                    <img src={person_icon} alt=""/>
+                    <input type="text"/>
+                </div>
+                <div className="input">
+                    <img src={email_icon} alt=""/>
+                    <input type="email"/>
+                </div>
+                <div className="input">
+                    <img src={lock_icon} alt=""/>
+                    <input type="password"/>
+                </div>
             </div>
-
-            <div className="input">
-                <img src={email_icon} alt=""/>
-                <input type="email"/>
+            <div className= "forgot-password">Lost Password? <span>Click Here!</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
             </div>
-
-            <div className="input">
-                <img src={lock_icon} alt=""/>
-                <input type="password"/>
-            </div>
-
         </div>
-    )
+    );
 }
 
 export default LoginSignup
