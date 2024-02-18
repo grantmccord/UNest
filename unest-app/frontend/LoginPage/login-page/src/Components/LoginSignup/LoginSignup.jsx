d74406232652fddecfda89eb752771c8b2ec1554
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-02-18 00:29:26 -0500

    Added clicking functionality -Sud

--- unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
+++ unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginSignup.css'
 
 import email_icon from '../Assets/email.png'
 import lock_icon from '../Assets/lock.png'
 import person_icon from '../Assets/person.png'
 const LoginSignup = () => {
+    const [action,setAction] = useState("Login");
     return (
         <div className='container'>
             <div className="header">
-                <div className="text">Registration</div>
+                <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
@@ -27,8 +28,10 @@ const LoginSignup = () => {
             </div>
             <div className= "forgot-password">Forgot Password? <span>Click Here!</span></div>
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action==="Login"?"submit gray":"submit"}
+                     onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Sign Up"?"submit gray":"submit"}
+                     onClick={()=>setAction("Login")}>Login</div>
             </div>
         </div>
     );
