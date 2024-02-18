14099eb9c2b1f5b9d8ac67dfc769b7f2621189c1
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-02-17 21:31:11 -0500

    Created registration with 3 fields-Sud

--- unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
+++ unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
@@ -8,7 +8,7 @@ const LoginSignup = () => {
     return (
         <div className='container'>
             <div className="header">
-                <div className="text">Sign Up</div>
+                <div className="text">Registration</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
