2a47ad9daea6808f3cafc80193c76c393e02b212
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-02-18 00:56:12 -0500

    Title added with functionality of text boxes -Sud

--- unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
+++ unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.jsx
@@ -7,34 +7,47 @@ import person_icon from '../Assets/person.png'
 const LoginSignup = () => {
     const [action,setAction] = useState("Login");
     return (
-        <div className='container'>
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                {action === "Login" ? <div></div> : <div className="input">
-                    <img src={person_icon} alt=""/>
-                    <input type="text" placeholder="Name"/>
-                </div>}
+        <div className='outercontainer'>
 
-                <div className="input">
-                    <img src={email_icon} alt=""/>
-                    <input type="email" placeholder="University Email"/>
-                </div>
-                <div className="input">
-                    <img src={lock_icon} alt=""/>
-                    <input type="password" placeholder="Password"/>
+            <div className='title'>
+                <div className="welcome">
+                    <div className="txt">Welcome to UNest!</div>
                 </div>
             </div>
-            {action === "Sign Up" ? <div></div> :
-                <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
 
-            <div className="submit-container">
-                <div className={action==="Login"?"submit gray":"submit"}
-                     onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray":"submit"}
-                     onClick={()=>setAction("Login")}>Login</div>
+            <div className='container'>
+                <div className="header">
+                    <div className="text">{action}</div>
+                    <div className="underline"></div>
+                </div>
+                <div className="inputs">
+                    {action === "Login" ? <div></div> : <div className="input">
+                        <img src={person_icon} alt=""/>
+                        <input type="text" placeholder="Name"/>
+                    </div>}
+
+                    <div className="input">
+                        <img src={email_icon} alt=""/>
+                        <input type="email" placeholder="University Email"/>
+                    </div>
+                    <div className="input">
+                        <img src={lock_icon} alt=""/>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                </div>
+                {action === "Sign Up" ? <div></div> :
+                    <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
+
+                <div className="submit-container">
+                    <div className={action === "Login" ? "submit gray" : "submit"}
+                         onClick={() => {
+                             setAction("Sign Up")
+                         }}>Sign Up
+                    </div>
+                    <div className={action === "Sign Up" ? "submit gray" : "submit"}
+                         onClick={() => setAction("Login")}>Login
+                    </div>
+                </div>
             </div>
         </div>
     );
