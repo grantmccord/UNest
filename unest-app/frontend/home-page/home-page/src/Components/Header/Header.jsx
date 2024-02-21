6e1d2ded3c93f0d35e4d2e3570eb02b74e5cd994
grantmccord <gmccord6502@comcast.net>
2024-02-20 21:04:33 -0500

    Downloaded Tailwind and improved header.
    Download Tailwind and made some improvements to the header.

--- unest-app/frontend/home-page/home-page/src/Components/Header/Header.jsx
+++ unest-app/frontend/home-page/home-page/src/Components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import './Header.css';
 import React from 'react';
 import MessagesButton from "../MessagesButton/MessagesButton";
+import SearchBar from "../SearchBar/SearchBar"
 
 const Header = () => {
     return(
       <div className='header'>
-        <div className='overlap'>
-            <h1 className='title'>
-                UNest
-            </h1>
+        <div className='buttonWidthElement'>
+            <SearchBar/>
         </div>
-        <div className='overlap msg'>
+        <h1 className='title'>
+            UNest
+        </h1>
+        <div className='buttonWidthElement'>
             <MessagesButton/>
         </div>
       </div>
