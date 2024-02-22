426111548a7392c24d4bbd42241db6f983f65e9e
Nivedha Kumar <to.nivedha75@gmail.com>
2024-02-22 02:08:45 -0500

    created a grid of properties on the homepage with correct spacing and aligning

---
+++ unest-app/frontend/home-page/home-page/src/Components/Property/Property.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./Property.css";
+import Button from "@mui/material/Button";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Box, { BoxProps } from "@mui/material/Box";
+
+import dormroom1 from "../Assets/dorm-room1.jpg";
+
+const Property = () => {
+    return (
+        <Card sx={{ maxWidth: 345, maxHeight: 345, boxShadow: 7 }} className="card">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={dormroom1}
+                    alt="property"
+                />
+                <CardContent>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Typography gutterBottom variant="h6" component="div">
+                            2 Bed Room at Granite Apartments
+                        </Typography>
+                        <Typography variant="body1" mt={1} ml={3} color="text.secondary">
+                            $849/month
+                        </Typography>
+                    </Box>
+
+                    <Typography variant="body2" color="text.secondary">
+                        Jan 7th - May 4th 2024
+                    </Typography>
+
+                    <Typography variant="body2" mt={1} color="text.secondary">
+                        0.1 miles away
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default Property;
