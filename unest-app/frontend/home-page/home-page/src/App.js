2619b3ecf59d2211631bf5e015b601092b898a65
grantmccord <gmccord6502@comcast.net>
2024-02-23 22:33:19 -0500

    Added Tailwind config and a Search Bar
    Installed Tailwind but I am having some issues using it in the project. I created a new folder which I have successfully installed Tailwind in and will be using that new folder to add my changes until we refactor the repository.

--- unest-app/frontend/home-page/home-page/src/App.js
+++ unest-app/frontend/home-page/home-page/src/App.js
@@ -4,7 +4,10 @@ import Header from "./Components/Header/Header"
 function App() {
   return (
     <div>
-      <Header/>
+      <div className="bg-red-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+        <span>Search</span>
+      </div>
+      {/* <Header/> */}
       {/* <MessagesButton/> */}
     </div>
   );
