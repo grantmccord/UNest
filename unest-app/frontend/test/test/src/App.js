8173c0ce44ffe8e43aca2d4608f30c1a5b212cdd
grantmccord <gmccord6502@comcast.net>
2024-02-24 21:55:29 -0500

    Adjusted Header
    Header now includes the word UNest on the left, a search bar and filter in the middle, and a messages button on the right.

--- unest-app/frontend/test/test/src/App.js
+++ unest-app/frontend/test/test/src/App.js
@@ -4,32 +4,7 @@ import HeaderTest from "./HeaderTest.jsx";
 
 function App() {
   return (
-    <HeaderTest/>
-    // <div class="bg-red-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
-    //   <span>Search</span>
-    // </div>
-    // <div class="min-h-screen bg-gray-100 flex justify-center items-center px-20">
-    //   <div class="space-y-10">
-    //     <div class="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
-    //       <div class="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
-    //         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    //           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-    //         </svg>
-    //         <input class="bg-gray-100 outline-none" type="text" placeholder="Article name or keyword..." />
-    //       </div>
-    //       <div class="flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointer">
-    //         <span>All categories</span>
-
-    //         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    //           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-    //         </svg>
-    //       </div>
-    //       <div class="bg-red-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
-    //         <span>Search</span>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    <Header/>
   );
 }
 
--- unest-app/frontend/test/test/src/Header.jsx
+++ unest-app/frontend/test/test/src/Header.jsx
@@ -1,14 +1,20 @@
 import { Button } from "@material-tailwind/react";
-import SearchBar from "./SearchBar.jsx"
+import SearchBar from "./SearchBar.jsx";
+import email from "./email.png";
 
 const Header = () => {
     return (
         <div>
             <header className="sticky top-0">
-                <ul>
+                <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    {/* <Button class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">name</Button> */}
+                    <h1 className="font-oswald text-primary-800 mb-4 text-4xl font-medium">UNest</h1>
                     <SearchBar/>
-                    <Button variant = "contained" color = "primary" size = 'large' verticalAlign = 'align-middle'>test</Button>
-                </ul>
+                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                        <img src = {email} width = {25} height = {25} alt = ""/>
+                        <span>Messages</span>
+                    </button>
+                </nav>
             </header>
         </div>
     );
