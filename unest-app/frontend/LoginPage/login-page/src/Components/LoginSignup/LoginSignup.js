bb8c0eec674a3308a88afb0b815b5c7aa1acac14
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-02-25 18:50:51 -0500

    Combined both registrations

--- unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.js
+++ unest-app/frontend/LoginPage/login-page/src/Components/LoginSignup/LoginSignup.js
@@ -7,6 +7,72 @@ import person_icon from '../Assets/person.png'
 import school_icon from '../Assets/school.png'
 const LoginSignup = () => {
     const [action,setAction] = useState("Login");
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        birthday: "",
+        password: "",
+        confirmPassword: "",
+    });
+    const inputs = [
+        {
+            id: 1,
+            name: "username",
+            type: "text",
+            placeholder: "Username",
+            errorMessage:
+                "Username should be 3-16 characters and shouldn't include any special character!",
+            label: "Username",
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true,
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "email",
+            placeholder: "Email",
+            errorMessage: "It should be a valid email address!",
+            label: "Email",
+            required: true,
+        },
+        {
+            id: 3,
+            name: "birthday",
+            type: "date",
+            placeholder: "Birthday",
+            label: "Birthday",
+        },
+        {
+            id: 4,
+            name: "password",
+            type: "password",
+            placeholder: "Password",
+            errorMessage:
+                "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+            label: "Password",
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+            required: true,
+        },
+        {
+            id: 5,
+            name: "confirmPassword",
+            type: "password",
+            placeholder: "Confirm Password",
+            errorMessage: "Passwords don't match!",
+            label: "Confirm Password",
+            pattern: values.password,
+            required: true,
+        },
+    ];
+
     return (
         <div className='outercontainer'>
             <div className='container'>
@@ -14,7 +80,7 @@ const LoginSignup = () => {
                     <div className="text">{action}</div>
                     <div className="underline"></div>
                 </div>
-                <div className="inputs">
+                <form className="inputs">
                     {action === "Login" ? <div></div> : <div className="input">
                         <img src={person_icon} alt=""/>
                         <input type="text" placeholder="First Name"/>
@@ -38,8 +104,7 @@ const LoginSignup = () => {
                         <img src={lock_icon} alt=""/>
                         <input type="password" placeholder="Password"/>
                     </div>
-
-                </div>
+                </form>
                 {action === "Sign Up" ? <div></div> :
                     <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
 
