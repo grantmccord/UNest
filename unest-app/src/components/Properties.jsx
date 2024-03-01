9352d6d42672fadde159c88dfa6a8b71c0e3fb8e
Nivedha Kumar <to.nivedha75@gmail.com>
2024-03-01 03:16:33 -0500

    test push

--- unest-app/src/components/Properties.jsx
+++ unest-app/src/components/Properties.jsx
@@ -5,6 +5,7 @@ import Property from "./Property.jsx";
 import Grid from '@mui/material/Grid';
 
 export const Properties = () => {
+
     return (
         <Grid data-testid="propertyGrid" container sx={{ justifyContent: 'space-around', py: 6, px: 8 }} rowSpacing={4} columns={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
             <Grid item role="item">
