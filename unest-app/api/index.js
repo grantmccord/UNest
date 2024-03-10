4d437987db0e9822b3301fa701da5f843144d097
Sudhanva02 <sudhanvabharadwaj@gmail.com>
2024-03-10 12:03:58 -0400

    Added api for uploading images

--- unest-app/api/index.js
+++ unest-app/api/index.js
@@ -39,6 +39,30 @@ app.post('/register', async (req,res) =>{
         res.status(422).json(e);
     }
 
+
+
+    app.post('/api/upload-by-link', async (req,res) => {
+        const {link} = req.body;
+        const newName = 'photo' + Date.now() + '.jpg';
+        await imageDownloader.image({
+            url: link,
+            dest: '/tmp/' +newName,
+        });
+        const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
+        res.json(url);
+    });
+
+    const photosMiddleware = multer({dest:'/tmp'});
+    app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+        const uploadedFiles = [];
+        for (let i = 0; i < req.files.length; i++) {
+            const {path,originalname,mimetype} = req.files[i];
+            const url = await uploadToS3(path, originalname, mimetype);
+            uploadedFiles.push(url);
+        }
+        res.json(uploadedFiles);
+    });
+
 });
 
 
