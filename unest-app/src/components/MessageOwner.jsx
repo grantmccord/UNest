58e806436c12aa7102415558052f885535b7207b
coderRamL <rlaxmina@purdue.edu>
2024-03-09 21:51:15 -0500

    Designed top half of a user's specific chat.

--- unest-app/src/components/MessageOwner.jsx
+++ unest-app/src/components/MessageOwner.jsx
@@ -1,6 +1,29 @@
+import './MessageOwner.css'
+import back from '../Assets/back.png'
+import profileIcon from '../Assets/Profile.png';
+
 const MessageOwner = () => {
     return (
-        <h1>Property Owner</h1>
+        <div>
+        <div className='back'>
+        <img src={back} alt="" style={{width: "70px", height: "70px"}} />
+        </div>
+        <div className='prof'>
+        <img src={profileIcon} alt="" style={{width: "70px", height: "70px"}} />
+        </div>
+        <div className="name">
+        <h1>Owner Name</h1>
+        <p style={{position: "relative", top: "30px", left: "-165px", fontSize: "30px"}}>username</p>
+        </div>
+        <hr style={{display: "flex", position: "relative", top: "-110px", color: "gray"}}/>
+        <div className="mes1">
+        <button style={{backgroundColor: "#EA5455", color: "black", width: "700px", height: "100px", textDecoration: "none"}}>
+        <img src={profileIcon} alt="" style={{width: "50px", height: "50px"}} />
+        <p style={{position: "relative", top: "-40px"}}>Owner Name</p>
+        <p style={{position: "relative", top: "-40px"}}>Do you have any questions?</p>
+        </button>
+        </div>
+        </div>
     );
 };
 
