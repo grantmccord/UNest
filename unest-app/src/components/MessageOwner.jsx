863542aa9ce5ee3e8b746c7ebaee9992a080afbd
coderRamL <rlaxmina@purdue.edu>
2024-03-09 22:28:33 -0500

    Designed rest of user's specific chat with owner.

--- unest-app/src/components/MessageOwner.jsx
+++ unest-app/src/components/MessageOwner.jsx
@@ -19,9 +19,17 @@ const MessageOwner = () => {
         <div className="mes1">
         <button style={{backgroundColor: "#EA5455", color: "black", width: "700px", height: "100px", textDecoration: "none"}}>
         <img src={profileIcon} alt="" style={{width: "50px", height: "50px"}} />
-        <p style={{position: "relative", top: "-40px"}}>Owner Name</p>
         <p style={{position: "relative", top: "-40px"}}>Do you have any questions?</p>
         </button>
+        </div>
+        <div className="mes2">
+        <button style={{backgroundColor: "#EA5455", color: "black", width: "700px", height: "100px", textDecoration: "none"}}>
+        <p style={{position: "relative", top: "20px"}}>Yes, what is the surrounding like?</p>
+        <img src={profileIcon} alt="" style={{width: "50px", height: "50px", position: "relative", top: "-30px", left: "630px"}} />
+        </button>
+        </div>
+        <div className="search">
+        <input type="text" value="Message" style={{width: "1200px", textAlign: "center", position: "relative", left:"-185px"}}/>
         </div>
         </div>
     );
