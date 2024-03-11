1ca438d0971da7afcb9914e71ef8a3c0cc9ca2af
Nivedha Kumar <to.nivedha75@gmail.com>
2024-03-10 18:24:41 -0700

    changed data displayed on homepage

--- unest-app/src/components/Properties.jsx
+++ unest-app/src/components/Properties.jsx
@@ -27,7 +27,7 @@ export const Properties = () => {
     // };
 
     //const listings = [{ "_id": { "$oid": "65c95c0270d91fbd7c84bbf1" }, "name": "2 Bed Room at Granite", "price": { "$numberInt": "849" }, "start_date": { "$date": { "$numberLong": "1704585600000" } }, "end_date": { "$date": { "$numberLong": "1714780800000" } }, "miles_from_campus": { "$numberDecimal": "0.1" }, "time": { "$date": { "$numberLong": "1707609600000" } }, "address": "Granite Student Living, 225 Northwestern Ave, West Lafayette, IN 47906", "university": "Purdue University", "rating": { "$numberDecimal": "4.3" }, "description": "An amazing place to stay.", "num_rooms": { "$numberDecimal": "0.5" }, "num_baths": { "$numberDecimal": "0.5" }, "total_rooms": { "$numberInt": "2" }, "total_baths": { "$numberInt": "2" }, "amenities": ["In-Unit Laundry", "Garage Parking", "Gym"], "roommate_group": ["65c96fe2b2fe68c739c569ac"] }]
-    const listings = [{ "name": "2 Bed Room at Granite", "price": 849, startDate: "1/2/2003", endDate: "1/3/2003", "milesFromCampus": 0.1 }, { "name": "Aspire", "price": 1000, startDate: "2/2/2003", endDate: "2/3/2003", "milesFromCampus": 0.5 }]
+    const listings = [{ "name": "2 Bed Room at Granite", "price": 849, startDate: "1/2/2023", endDate: "1/3/2023", "milesFromCampus": 0.1 }, { "name": "4 Bed Room at Aspire", "price": 1000, startDate: "2/2/2023", endDate: "2/3/2023", "milesFromCampus": 0.5 }]
 
     return (
         <Grid data-testid="propertyGrid" container sx={{ justifyContent: 'space-around', py: 6, px: 8 }} rowSpacing={4} columns={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
