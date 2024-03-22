f1a39d4ebd5e243e98cb945b2eaad44f8a14f993
grantmccord <gmccord6502@comcast.net>
2024-03-21 20:28:56 -0400

    The filter button now includes a dropdown with options
    Successfully updated the filter button so that on click it now opens a dropdown menu with three options: a Bedroom Count button, a Room Type button, and a Price section with a slider to select a max price.

--- unest-app/src/components/Filter.jsx
+++ unest-app/src/components/Filter.jsx
@@ -1,15 +1,75 @@
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import SliderComponent from './SliderComponent';
+// import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
-const Filter = () => {
-    return (
-        <div className="flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointer">
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Options
+          {/* <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" /> */}
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Bedroom Count
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Room Type
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="py-1 px-5">
+            <Menu.Item>
+              <div className='text-gray-700 block px-4 py-2 text-sm'>
+                Price
+                <SliderComponent />
+              </div>
+              
+              {/* {({ active }) => (
                 
-            <div>Filter</div>
-    
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-            </svg>
-        </div>
-    )
-};
-
-export default Filter;
+              )} */}
+            </Menu.Item>
+          </div>
+          
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
