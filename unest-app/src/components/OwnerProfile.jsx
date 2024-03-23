cce95fe8f1c977995201208943c348433e4122fc
Nivedha Kumar <to.nivedha75@gmail.com>
2024-03-23 17:36:30 -0400

    made the about me field in profile page editable (with pencil edit icon, can switch between edit mode with text field and regular text)

--- unest-app/src/components/OwnerProfile.jsx
+++ unest-app/src/components/OwnerProfile.jsx
@@ -1,23 +1,80 @@
-import {
-    useNavigate,
-  } from "react-router-dom";
+// import {
+//     useNavigate,
+//   } from "react-router-dom";
 
-const OwnerProfile = () => {
-    const navigate = useNavigate();
-    const navigateToOwner = () => {
-        navigate('/messageOwner', {replace: true})
-    } 
+// const OwnerProfile = () => {
+//     const navigate = useNavigate();
+//     const navigateToOwner = () => {
+//         navigate('/messageOwner', {replace: true})
+//     } 
+//     return (
+//         <div>
+//         <div>
+//         <h1>Owner Profile</h1>
+//         </div>
+//         <div>
+//             <button onClick={navigateToOwner} style={{position: "relative", top: "-65px", left: "1100px", width: "100px", height: "50px"}}>Message</button>
+//         </div>
+//         </div>
+
+//     );
+// };
+
+// export default OwnerProfile;
+
+
+import React, { useState } from 'react';
+import { TextField, Typography } from '@mui/material';
+
+function EditableText(props) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState(props.initialText);
+
+    const handleDoubleClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleBlur = () => {
+        setIsEditing(false);
+        // Here you might want to save the edited text, for example by calling a function passed as a prop
+        if (props.onTextChange) {
+            props.onTextChange(text);
+        }
+    };
+
     return (
         <div>
-        <div>
-        <h1>Owner Profile</h1>
+            {isEditing ? (
+                <TextField
+                    value={text}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    autoFocus
+                />
+            ) : (
+                <Typography variant="body1" onDoubleClick={handleDoubleClick}>{text}</Typography>
+            )}
         </div>
-        <div>
-            <button onClick={navigateToOwner} style={{position: "relative", top: "-65px", left: "1100px", width: "100px", height: "50px"}}>Message</button>
-        </div>
-        </div>
-
     );
-};
+}
 
-export default OwnerProfile;
+function App() {
+    const [displayText, setDisplayText] = useState("Click me to edit!");
+
+    const handleTextChange = (newText) => {
+        setDisplayText(newText);
+    };
+
+    return (
+        <div className="App">
+            <h1>Editable Text Demo</h1>
+            <EditableText initialText={displayText} onTextChange={handleTextChange} />
+        </div>
+    );
+}
+
+export default App;
--- unest-app/src/components/ProfilePage.jsx
+++ unest-app/src/components/ProfilePage.jsx
@@ -1,9 +1,71 @@
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+
+function EditableText(props) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState(props.initialText);
+
+    const handleDoubleClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleBlur = () => {
+        setIsEditing(false);
+        // Here you might want to save the edited text, for example by calling a function passed as a prop
+        if (props.onTextChange) {
+            props.onTextChange(text);
+        }
+    };
+
+    return (
+        <div>
+            {isEditing ? (
+                <TextField
+                    value={text}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    autoFocus
+                />
+            ) : (
+                <Typography variant="body1" onDoubleClick={handleDoubleClick}>{text}</Typography>
+            )}
+        </div>
+    );
+}
+
 
 const ProfilePage = () => {
+    const [isEditingAboutMe, setIsEditingAboutMe] = useState(false);
+    const [aboutMeText, setAboutMeText] = useState("This is About Me Description!");
+    const [editedAboutMeText, setEditedAboutMeText] = useState("This is About Me Description!");
+
+    const handleAboutMeEdit = () => {
+        console.log("setIsEditingAboutMe is set to true");
+        setIsEditingAboutMe(true);
+    };
+
+    const handleAboutMeSave = () => {
+        setIsEditingAboutMe(false);
+        setAboutMeText(editedAboutMeText);
+    };
+
+    const handleAboutMeCancel = () => {
+        setIsEditingAboutMe(false);
+        setEditedAboutMeText(aboutMeText);
+    };
+
+    const handleAboutMeChange = (e) => {
+        setEditedAboutMeText(e.target.value);
+    };
+
     return (
         <div>
             <Typography variant="h3" sx={{ textAlign: "center", pt: 3 }} gutterBottom>
@@ -16,12 +78,34 @@ const ProfilePage = () => {
                     </Typography>
                 </Grid>
                 <Grid item md={6}>
-                    <Typography variant="h4" gutterBottom>
-                        About Me
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
+                    <div className='aboutMe'>
+                        <Typography variant="h4" gutterBottom>
+                            About Me
+                        </Typography>
+                        <ModeEditIcon fontSize="small" color="black" sx={{ marginLeft: 1 }} onClick={() => handleAboutMeEdit()}></ModeEditIcon>
+                    </div>
+                    <div>
+                        {isEditingAboutMe ? (
+                            <>
+                                <TextField
+                                    value={editedAboutMeText}
+                                    onChange={handleAboutMeChange}
+                                    autoFocus
+                                />
+                                <Button onClick={handleAboutMeSave}>Save</Button>
+                                <Button onClick={handleAboutMeCancel}>Cancel</Button>
+                            </>
+                        ) : (
+                            <>
+                                <Typography variant="body1" gutterBottom>{aboutMeText}</Typography>
+                            </>
+                        )}
+                    </div>
+                    {/* <Typography variant="body1" gutterBottom>
                         This is about me description
-                    </Typography>
+                    </Typography> */}
+
+                    {/* <EditableText initialText={aboutMeText} onTextChange={handleAboutMeTextChange} /> */}
                 </Grid>
                 <Grid item md={6}>
                     <div>
@@ -31,8 +115,8 @@ const ProfilePage = () => {
                         <Typography variant="h5" gutterBottom>
                             21/Female
                         </Typography>
-                        <Button variant="contained">Message</Button>
-                        <Button variant="outlined">Properties Viewed</Button>
+                        <Button variant="contained" my={3} fullWidth={false}>Message</Button>
+                        <Button variant="outlined" my={3} >Properties Viewed</Button>
                     </div>
                 </Grid>
 
@@ -52,7 +136,7 @@ const ProfilePage = () => {
 
                 </Grid>
             </Grid>
-        </div>
+        </div >
 
 
 
