912a792f03ecd6352222a814580ec81175137490
grantmccord <gmccord6502@comcast.net>
2024-03-28 20:23:16 -0400

    Filter now receives values
    Whenever any of the options in the filter are clicked, the search button now properly receives the values and stores them.

--- unest-app/src/components/SearchBar.jsx
+++ unest-app/src/components/SearchBar.jsx
@@ -1,44 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Filter from "./Filter.jsx";
+import SliderComponent from './SliderComponent.jsx';
+import { slider } from '@material-tailwind/react';
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '', // Initialize the state with an empty value
-    };
+function SearchBar() {
+  const [inputValue, setInputValue] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [sliderValue, setSliderValue] = useState(2000); //max value a property can have
+
+  function handleChange(data) {
+    // Update the state when input value changes
+    setInputValue(data);
+  };
+
+  function handleFilter(data) {
+    setFilterValue(data);
   }
 
-  handleChange = (e) => {
-    // Update the state when input value changes
-    this.setState({ inputValue: e.target.value });
-  };
+  function handleSlider(data) {
+    setSliderValue(data);
+  }
 
-  handleClick = () => {
+  function handleClick() {
     // Access the input value when the button is clicked
-    alert('Input value is: ' + this.state.inputValue);
+    alert('Input value is: ' + inputValue + '\nFilter value is:' + filterValue + '\nSlider value is: ' + sliderValue);
   };
 
-  render() {
-    return <>
-        <div>
-          <div className="space-y-10">
-            <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
-              <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-                <input className="bg-gray-100 outline-none" onChange={this.handleChange} type="text" placeholder="Property Name" />
-              </div>
-              <Filter/>
-              <div onClick={this.handleClick} className="bg-red-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
-                <div>Search</div>
-              </div>
-            </div>
+
+
+  return <>
+    <div>
+      <div className="space-y-10">
+        <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
+          <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+            <input className="bg-gray-100 outline-none" onChange={handleChange} type="text" placeholder="Property/University Name" />
+          </div>
+          <Filter handleFilter={handleFilter} handleSlider={handleSlider} sliderValue={sliderValue}/>
+          <div onClick={handleClick} className="bg-red-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+            <div>Search</div>
           </div>
         </div>
-      </>
-  }
+      </div>
+    </div>
+  </>
 }
 
 export default SearchBar;
--- unest-app/src/components/SliderComponent.jsx
+++ unest-app/src/components/SliderComponent.jsx
@@ -4,15 +4,30 @@ import Slider from '@mui/material/Slider';
 // import '@mui/material/Slider/dist/Slider.css';
 
 const SliderComponent = ({ value, onChange }) => {
+  const marks = [
+    {
+      value: 300,
+      label: '$300'
+    },
+    {
+      value: 1150,
+      label: '$1150'
+    },
+    {
+      value: 2000,
+      label: '$2000'
+    },
+  ]
+
   return (
     <Slider
       value={value}
       onChange={onChange}
       min={300}
       max={2000}
-      step={25}
       defaultValue={2000}
-      valueLabelDisplay="auto"
+      valueLabelDisplay="off"
+      marks={marks}
     />
   );
 };
