7672b83ae0d29b20b4c6687558318af58472727d
grantmccord <gmccord6502@comcast.net>
2024-04-14 13:00:26 -0400

    Fixed bug
    Fixed the bug where the markers would be invisible. Markers will now show up on the map corresponding to all of the different listings.

--- unest-app/src/components/HomeMap.jsx
+++ unest-app/src/components/HomeMap.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ21jY29yZDY1MDIiLCJhIjoiY2x1bHpxeDVjMTg5cTJqbzN5bGozbzgwaiJ9.-TGy73Y_CF2rZEBKGIKTJw';
 
@@ -11,22 +12,24 @@ export default function HomeMap({ listings }) {
     const [zoom, setZoom] = useState(14);
 
     var el = document.createElement('div');                                       
-    el.style.backgroundImage = 'url(https://placekitten.com/g/50/)';              
+    el.style.backgroundColor = '#FF0000';              
     el.style.width = '50px';                                                      
     el.style.height = '50px';
-    el.style.borderRadius = '50%';  
+    el.style.borderRadius = '50%';
+    el.style.opacity = 1;  
 
     const addresses = listings.map((listing) => listing.address);
 
     // ONLY WORKS AS LONG AS THE URL IS LESS THAN 256 CHARACTERS
     // only checks the first two listings in the database
-    const setMarkers = async () => {
+    const setMarkers = async (address) => {
       try {
         // console.log(addresses.join(";"))
-        const response = await fetch(encodeURI("https://api.mapbox.com/geocoding/v5/mapbox.places/" + addresses.slice(0,2).join(";") + ".json?access_token=pk.eyJ1IjoiZ21jY29yZDY1MDIiLCJhIjoiY2x1bHpxeDVjMTg5cTJqbzN5bGozbzgwaiJ9.-TGy73Y_CF2rZEBKGIKTJw"));
+        const response = await fetch(encodeURI("https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1IjoiZ21jY29yZDY1MDIiLCJhIjoiY2x1bHpxeDVjMTg5cTJqbzN5bGozbzgwaiJ9.-TGy73Y_CF2rZEBKGIKTJw"));
         // console.log(response)
         const data = await response.json();
-        new mapboxgl.Marker(el).setLngLat(data.features[0].center).addTo(map.current);
+        console.log(data)
+        new mapboxgl.Marker().setLngLat(data.features[0].center).addTo(map.current);
         // data.features.map((pair) => setCoords(pair.center))
         // console.log(data.features[0].center);
       } catch (error) {
@@ -50,9 +53,9 @@ export default function HomeMap({ listings }) {
         setZoom(map.current.getZoom().toFixed(2));
       });
       
-      new mapboxgl.Marker(el).setLngLat([-86.929, 40.424]).addTo(map.current);
-
-      setMarkers();
+      // new mapboxgl.Marker(el).setLngLat([-86.929, 40.424]).addTo(map.current);
+      new mapboxgl.Marker().setLngLat([-86.929, 40.424]).setPopup(new mapboxgl.Popup({ offset: 20 }).setHTML(<> HTML sample text </>)).addTo(map.current);
+      addresses.forEach((address) => setMarkers(address));
     });
   
     return (
